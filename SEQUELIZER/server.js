fe9fc2e157e2f48eb168f3dcf37eb89ee9ef9f2c
Rachel <hello.rachelwong@gmail.com>
2019-05-31 12:53:08 +1000

    updated sequelizer

--- SEQUELIZER/server.js
+++ SEQUELIZER/server.js
@@ -1,7 +1,7 @@
 //basic server set up
 const express = require("express")
 // const studentRoutes = require("./routes/students")
-const Sequelizer = require("sequelize")
+const Sequelize = require("sequelize")
 const bodyParser = require("body-parser")
 
 // initialise a students array
@@ -10,11 +10,13 @@ const students = ["Natasha", "Joe", "Matt", "Rachel", "Glen", "Dana"]
 const app = express() // pull in express
 const port = 3000
 
+// TIP: PUT ALL OF THE SEQUELIZE config into a db.js and require it into the main file
+
 // INITIALISE AN INSTANCE OF SEQUELIZER - VERSION 1
 // sequelize won't create db automatically. manual process
 // create the database from pg admin. sequelize will look after the defintiion of the database
 // need to pass four things: name of database, username, password, {object containing configuration information}
-const sequelize = new Sequelizer("nodetest", "postgres", "postgres", {
+const sequelize = new Sequelize("nodetest", "postgres", "postgres", {
 	host: "localhost",
 	dialect: "postgres" // dialect specify what database system we are using (mysql, mariadb, msSQL, postgres, sqlite)
 	// port: 5432. This is not necessary if using the default setting port
@@ -37,10 +39,24 @@ sequelize
 		console.error("Unable to connect: ", err)
 	})
 
-// Create student model
+// Define student model
 // .define ('name of model/db table", {object containing attributes})
 const Student = sequelize.define("student", {
-	name: {}
+	name: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	dob: Sequelize.DATE,
+})
+// sync the physical database iwht the defined model
+// essentially a db:migrate 
+sequelize.sync().then(() => {
+	console.log('Models synced!')
+	//creates student object to insert into the student table
+	Student.create({
+		name: "Bob",
+		dob: "1986-10-24", //this is the acceptabel format for date 
+	})
 })
 
 let requestTime = (req, res, next) => {
