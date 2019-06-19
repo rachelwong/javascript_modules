6150aa82c6e4faeed5ff7a5adaedcdbb5383d5fa
Rachel <hello.rachelwong@gmail.com>
2019-06-20 08:37:17 +1000

    commit

--- REACT-INTRO/sandra/src/App.js
+++ REACT-INTRO/sandra/src/App.js
@@ -5,12 +5,40 @@ import { About } from "./components/About"
 import { Contact } from "./components/Contact"
 import { Fav } from "./components/Fav"
 import { Header } from "./components/Header"
+
 function App() {
+	const favourites = [
+		{
+			header: "Pizza",
+			imageUrl: "https://bit.ly/2rJ4rnn",
+			bestTimeToEat: "All the time",
+			bestPlace: "Queen Margharitas"
+		},
+		{
+			header: "Quesadillas",
+			imageUrl: "https://bit.ly/2ryI6bZ",
+			bestTimeToEat: "Afternoon Siesta",
+			bestPlace: "Mejico"
+		},
+		{
+			header: "Icecream Pancakes",
+			imageUrl: "https://bit.ly/2zUznp2",
+			bestTimeToEat: "Breaky",
+			bestPlace: "Pancakes on the Rocks"
+		},
+		{
+			header: "Loaded Fries",
+			imageUrl: "https://bit.ly/2GdfYVK",
+			bestTimeToEat: "Breaky",
+			bestPlace: "My House"
+		}
+	]
+
 	return (
 		<div>
 			<Header />
 			<About />
-			<Fav />
+			<Fav favouritesData={favourites} />
 			<Contact />
 		</div>
 	)
--- REACT-INTRO/sandra/src/components/Fav.jsx
+++ REACT-INTRO/sandra/src/components/Fav.jsx
@@ -2,80 +2,27 @@ import React from "react"
 
 // a functional componetn can accept an object called props which will contain all the attributes we have set in that element
 
-const favourites = [
-	{
-		header: "Pizza",
-		imageUrl: "https://bit.ly/2rJ4rnn",
-		bestTimeToEat: "All the time",
-		bestPlace: "Queen Margharitas"
-	},
-	{
-		header: "Quesadillas",
-		imageUrl: "https://bit.ly/2ryI6bZ",
-		bestTimeToEat: "Afternoon Siesta",
-		bestPlace: "Mejico"
-	},
-	{
-		header: "Icecream Pancakes",
-		imageUrl: "https://bit.ly/2zUznp2",
-		bestTimeToEat: "Breaky",
-		bestPlace: "Pancakes on the Rocks"
-	},
-	{
-		header: "Loaded Fries",
-		imageUrl: "https://bit.ly/2GdfYVK",
-		bestTimeToEat: "Breaky",
-		bestPlace: "My House"
-	}
-]
-
-export function Fav() {
-	// Set up page first while I work out the array
+export function Fav(props) {
 	return (
 		<div>
-			<section id='fav-foods'>
-				<h2>Favourite Foods</h2>
-				<div>
-					<h4>Pizza</h4>
-					<img src='https://bit.ly/2rJ4rnn' />
-					<p>
-						Best time to eat: <span>During the game</span>
-					</p>
-					<p>
-						Best place: <span>Queen Margharitas</span>
-					</p>
-				</div>
-				<div>
-					<h4>Quesadillas</h4>
-					<img src='https://bit.ly/2ryI6bZ' />
-					<p>
-						Best time to eat: <span>Afternoon Siesta</span>
-					</p>
-					<p>
-						Best place: <span>Mejico</span>
-					</p>
-				</div>
-				<div>
-					<h4>Icecream Pancakes</h4>
-					<img src='https://bit.ly/2zUznp2' />
-					<p>
-						Best time to eat: <span>Breaky</span>
-					</p>
-					<p>
-						Best place: <span>Pancakes on the Rocks</span>
-					</p>
-				</div>
-				<div>
-					<h4>Loaded Fries</h4>
-					<img src='https://bit.ly/2GdfYVK' />
-					<p>
-						Best time to eat: <span>During the game</span>
-					</p>
-					<p>
-						Best place: <span>My House</span>
-					</p>
-				</div>
-			</section>
+			<FavRow favouritesData={props.favouritesData} />
 		</div>
 	)
+}
+
+const FavRow = props => {
+	const entry = props.favouritesData.map(item => {
+		return (
+			<div>
+				<h4>{item.header}</h4>
+				<p>
+					Best time to eat: <span>{item.bestTimeToEat}</span>
+				</p>
+				<p>
+					Best place: <span>{item.bestPlace}</span>
+				</p>
+			</div>
+		)
+	})
+	return <div>{entry}</div>
 }
